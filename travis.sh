215b92de4280e313ab1f7bacb1838f47c8595e7a
Julian K. Arni <jkarni@gmail.com>
2016-10-04 15:18:21 +0200

    Raise lower bounds.
        These aren't being tested and aren't supported.
        Fix travis for servant-0.7.

--- travis.sh
+++ travis.sh
@@ -8,4 +8,5 @@ stack build --ghc-options=-Werror --no-terminal --flag servant-matrix-param:with
 stack build --ghc-options=-Werror --no-terminal --flag servant-matrix-param:-with-servant-aeson-specs --flag servant-matrix-param:with-servant-server
 stack build --ghc-options=-Werror --no-terminal --flag servant-matrix-param:-with-servant-aeson-specs --flag servant-matrix-param:-with-servant-server
 
-stack test  --ghc-options=-Werror --no-terminal --flag --stack-yaml=stack-servant-0.7.yaml
+stack setup --no-terminal --stack-yaml=stack-servant-0.7.yaml
+stack test  --ghc-options=-Werror --no-terminal --stack-yaml=stack-servant-0.7.yaml
