8100c82a432ae70492b809d9768ee3bc4b5c1408
Julian K. Arni <jkarni@gmail.com>
2016-10-19 15:07:22 +0200

    CPP the world, for servant-client 0.7 test support

--- test/Servant/MatrixParam/ClientSpec.hs
+++ test/Servant/MatrixParam/ClientSpec.hs
@@ -10,13 +10,11 @@ import Control.Exception
 import Control.Monad
 import Data.ByteString     (ByteString)
 import Data.IORef
-import Data.Maybe          (fromMaybe)
 import Data.Proxy
 import Network.HTTP.Client (Manager, Request, defaultManagerSettings,
                             managerModifyRequest, managerResponseTimeout,
                             newManager, path)
 import Servant
-import Network.Wai.Handler.Warp (testWithApplication)
 import Servant.Client
 import System.IO.Unsafe
 import Test.Hspec
@@ -24,6 +22,13 @@ import Test.Hspec
 #if MIN_VERSION_http_client(0,5,0)
 import Network.HTTP.Client (responseTimeoutMicro)
 #endif
+#if MIN_VERSION_servant_client(0,9,0)
+import Network.Wai.Handler.Warp (testWithApplication)
+import Data.Maybe          (fromMaybe)
+#else
+import Control.Monad.Trans.Except (runExceptT)
+#endif
+
 
 import Servant.MatrixParam
 import Servant.MatrixParam.Client ()
@@ -42,6 +47,7 @@ spec = do
       cliA Nothing `hasRequestPath` "/a"
       cliB Nothing Nothing `hasRequestPath` "/b"
 
+#if MIN_VERSION_servant_client(0,9,0)
     it "generates paths that servant-server understands" $ do
       testWithApplication (return $ serve api server) $ \port -> do
         let res = (,) <$> cliA (Just "There is a there there")
@@ -50,6 +56,7 @@ spec = do
         mgr' <- newManager defaultManagerSettings
         runClientM res (ClientEnv mgr' url)
           `shouldReturn` Right ("There is a there there", "3")
+#endif
 ------------------------------------------------------------------------------
 -- API
 ------------------------------------------------------------------------------
@@ -62,21 +69,33 @@ type Api =
 api :: Proxy Api
 api = Proxy
 
+
+#if MIN_VERSION_servant_client(0,9,0)
 server :: Server Api
 server = e1 :<|> e2
   where
     e1 name = return $ fromMaybe "" name
     e2 foo bar = return . show $ fromMaybe 0 foo + fromMaybe 0 bar
+#endif
 
 ------------------------------------------------------------------------------
 -- Utils
 ------------------------------------------------------------------------------
 
+#if MIN_VERSION_servant_client(0,9,0)
 hasRequestPath :: ClientM any -> ByteString -> IO ()
+#else
+hasRequestPath :: (Manager -> BaseUrl -> ClientM any) -> ByteString -> IO ()
+#endif
 hasRequestPath c expectedPath = do
   let anyBurl = BaseUrl Http "localhost" 6660 ""
+#if MIN_VERSION_servant_client(0,9,0)
   _ <- void (runClientM c $ ClientEnv mgr anyBurl)
     `catch` \(_ :: SomeException) -> return ()
+#else
+  _ <- void (runExceptT $ c mgr anyBurl)
+    `catch` \(_ :: SomeException) -> return ()
+#endif
   Just r <- readIORef lastRequest
   path r `shouldBe` expectedPath
 
@@ -99,6 +118,12 @@ mgr = unsafePerformIO . newManager $ defaultManagerSettings
 
 
 
+
+#if MIN_VERSION_servant_client(0,9,0)
 cliA :: Maybe String -> ClientM String
 cliB :: Maybe Int -> Maybe Int -> ClientM String
+#else
+cliA :: Maybe String -> Manager -> BaseUrl -> ClientM String
+cliB :: Maybe Int -> Maybe Int -> Manager -> BaseUrl -> ClientM String
+#endif
 cliA :<|> cliB = client api
