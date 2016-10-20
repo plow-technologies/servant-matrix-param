6d623b2e37ff0f724dc90b48b9c4093a2dd56569
Julian K. Arni <jkarni@gmail.com>
2016-10-20 19:59:31 +0200

    Add MatrixFlag support.

--- src/Servant/MatrixParam.hs
+++ src/Servant/MatrixParam.hs
@@ -6,6 +6,7 @@
 module Servant.MatrixParam (
   WithMatrixParams,
   MatrixParam,
+  MatrixFlag,
 ) where
 
 import           Data.Typeable (Typeable)
@@ -24,4 +25,20 @@ data WithMatrixParams (path :: Symbol) (paramSpecs :: [*])
 -- >>>
 -- >>> type MyApi = "books" :> MatrixParam "author" String :> Get '[JSON] [String]
 data MatrixParam (key :: Symbol) a
+    deriving (Typeable)
+
+-- | Lookup a potentially value-less matrix string parameter
+-- with boolean semantics. If the param @sym@ is there without any value,
+-- or if it's there with value "true" or "1", it's interpreted as 'True'.
+-- Otherwise, it's interpreted as 'False'.
+--
+--
+-- @/book;published@
+--
+-- would be represented as:
+--
+-- >>> import Servant
+-- >>>
+-- >>> type MyApi = "book" :> MatrixFlag "published" :> Get '[JSON] [String]
+data MatrixFlag (key :: Symbol)
     deriving (Typeable)
